dd1fec2c1d5942f437fe26c71230204326d1a6a1
jabre <marko13r@gmail.com>
2021-11-25 23:12:21 +0100

    initial commit

---
+++ src/main/java/com/jabre/spring5guru/Spring5guruApplication.java
@@ -0,0 +1,13 @@
+package com.jabre.spring5guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5guruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5guruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jabre/spring5guru/Spring5guruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabre.spring5guru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5guruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
